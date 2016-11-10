8c765d1508ec2a01721bce29f7e1bb08957e698c
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2016-11-10 23:29:09 +0000

    load page and check for correct title

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ try:
     browser = initialise_browser()
 except Exception as e:
     print("Not able to initialise browser object - {}".format(e))
+
+browser.get('http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk')
+
+# possibly not the best test since title my change but if this does happen may mean format
+# of whole page has changed...so good to know!
+assert browser.title == 'O2 | International | International Caller Bolt On'
